dd9a262fcfcaae57d4671355e4c89bc809b2d32a
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-07-01 12:13:31 +0530

    update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
+const userRoutes = require('./routes/userRoutes');
+const roleRoutes = require('./routes/roleRoutes');
 const connectDB = require('../Backend/config/connectDatabase');
 dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 
 connectDB();
 app.use(bodyParser.json());
 
+app.use('/api', userRoutes);
+app.use('/api', roleRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Not found' });
